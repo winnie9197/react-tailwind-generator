3e9279cbf83c7e42d8cd7b4e1c82e0fb182c4fd9
winniehcy <winniehcyeung@gmail.com>
2023-08-07 23:50:53 -0400

    adjust ui for prompt and response sections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import './App.css';
 import React, { useState } from 'react';
 import axios from 'axios';
-import 'tailwindcss/tailwind.css';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+
 const { Configuration, OpenAIApi } = require("openai");
 
 function App() {
@@ -15,6 +18,7 @@ function App() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
+        console.log(prompt);
         const res = await axios.post('http://localhost:3001/api/gpt-3', { prompt }); // This POSTs to your server
         setResponse(res.data); // Capture the response from your server
         setError(null);
@@ -24,34 +28,65 @@ function App() {
     }
 };
 
+  const onCodeChange = React.useCallback((value, viewUpdate) => {
+    console.log('value:', value);
+  }, []);
+
   return (
-    <div className="App bg-gray-100 min-h-screen p-6">
-        <div className="bg-white rounded-lg shadow-lg p-8 max-w-lg mx-auto">
-            <form className="mb-4" onSubmit={handleSubmit}>
-                <label className="block text-sm font-bold mb-2" htmlFor="prompt">
-                    Prompt:
-                </label>
-                <input 
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="prompt"
-                    type="text"
-                    value={prompt}
-                    onChange={handlePromptChange}
-                />
-                <button 
-                    className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                    type="submit"
-                >
-                    Submit
-                </button>
-            </form>
-            <div className="mb-4">
-                <p className="font-semibold">Response:</p>
-                <p className="border p-4 rounded">{response}</p>
-            </div>
-            {error && <p className="mt-4 text-red-600">{error}</p>}
-        </div>
-    </div>
+    <div className="flex flex-col h-screen bg-gray-300 items-center justify-center p-4">
+
+      {/* Main Container */}
+      <div className="bg-white p-8 rounded-2xl shadow-2xl w-full max-w-3xl space-y-8">
+
+          {/* Title */}
+          <h1 className="text-3xl font-semibold text-center text-gray-800 mb-6">Ask GPT</h1>
+          
+          {/* Prompt Section */}
+          <div className="space-y-4 border-b-2 pb-6">
+              <label className="block text-xl font-medium text-gray-700 mb-2">Prompt:</label>
+              <form className="space-y-4" onSubmit={handleSubmit}>
+                  <div>
+                      <textarea 
+                          className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 resize-none"
+                          rows="5"
+                          placeholder="Enter your prompt here..."
+                          value={prompt}
+                          onChange={handlePromptChange}
+                      ></textarea>
+                  </div>
+                  <div>
+                      <button 
+                          type="submit"
+                          className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 transition-colors"
+                      >
+                          Submit
+                      </button>
+                  </div>
+              </form>
+          </div>
+
+          {/* Response Section */}
+          <div className="space-y-4 mt-6">
+              <label className="block text-xl font-medium text-gray-700 mb-2">Response:</label>
+              <div className="flex-1 p-4 border rounded-lg bg-gray-100">
+                  <CodeMirror
+                      value={response || ""}
+                      height="300px"
+                      width="100%"
+                      extensions={[javascript({ jsx: true })]}
+                      onChange={onCodeChange}
+                  />
+              </div>
+          </div>
+      </div>
+
+      {error && (
+          <div className="mt-6 text-center">
+              <p className="text-red-500 font-medium">{error}</p>
+          </div>
+      )}
+  </div>
+
   );
 }
 
